b89633f68d7e179cf63bd953b0c10edaf69eb9f5
Rob Vesse <rvesse@dotnetrdf.org>
2015-05-21 11:46:49 +0100

    Basic example of a CLI

---
+++ examples/src/main/java/com/github/rvesse/airline/examples/cli/Help.java
@@ -0,0 +1,35 @@
+package com.github.rvesse.airline.examples.cli;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import com.github.rvesse.airline.Arguments;
+import com.github.rvesse.airline.Command;
+import com.github.rvesse.airline.examples.ExampleRunnable;
+import com.github.rvesse.airline.model.GlobalMetadata;
+
+@Command(name = "help", description = "A command that provides help on other commands")
+public class Help implements ExampleRunnable {
+
+    @Inject
+    private GlobalMetadata global;
+
+    @Arguments(description = "Provides the name of the commands you want to provide help from")
+    private List<String> commandNames = new ArrayList<String>();
+
+    @Override
+    public int run() {
+        try {
+            com.github.rvesse.airline.help.Help.help(global, commandNames);
+        } catch (IOException e) {
+            System.err.println("Failed to output help: " + e.getMessage());
+            e.printStackTrace(System.err);
+            return 1;
+        }
+        return 0;
+    }
+
+}
---
+++ examples/src/main/java/com/github/rvesse/airline/examples/cli/SimpleCli.java
@@ -0,0 +1,34 @@
+package com.github.rvesse.airline.examples.cli;
+
+import com.github.rvesse.airline.Cli;
+import com.github.rvesse.airline.Cli.CliBuilder;
+import com.github.rvesse.airline.examples.ExampleExecutor;
+import com.github.rvesse.airline.examples.ExampleRunnable;
+import com.github.rvesse.airline.examples.inheritance.Child;
+import com.github.rvesse.airline.examples.inheritance.GoodGrandchild;
+import com.github.rvesse.airline.examples.inheritance.Parent;
+import com.github.rvesse.airline.examples.simple.Simple;
+
+/**
+ * A simple example of creating a CLI
+ *
+ */
+public class SimpleCli {
+    
+    public static void main(String[] args) {
+        //@formatter:off
+        @SuppressWarnings("unchecked")
+        // The program name is cli
+        CliBuilder<ExampleRunnable> builder = Cli.<ExampleRunnable>builder("cli")
+                                                 // Add a description
+                                                 .withDescription("A simple CLI with several commands available")
+                                                 // Define some commands
+                                                 .withCommand(Simple.class)
+                                                 .withCommands(Parent.class, Child.class, GoodGrandchild.class)
+                                                 .withCommand(Help.class);
+        //@formatter:on
+        
+        ExampleExecutor.executeCli(builder.build(), args);
+    }
+
+}
