767796fd4c20f6dcda8eefcf10ebc0d460aada61
Rob Vesse <rvesse@dotnetrdf.org>
2015-10-23 16:04:32 +0100

    Stub for Markdown help generator module (#30)
    Adds the stub for the Markdown help generator module -
    airline-help-markdown - with implementations copied from the standard
    CLI implementation with minimal progress on conversion to Markdown
    output

---
+++ airline-help/airline-help-markdown/src/main/java/com/github/rvesse/airline/help/markdown/MarkdownCommandGroupUsageGenerator.java
@@ -0,0 +1,315 @@
+/**
+ * Copyright (C) 2010-15 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.rvesse.airline.help.markdown;
+
+import java.io.IOException;
+import java.util.*;
+
+import com.github.rvesse.airline.help.UsageHelper;
+import com.github.rvesse.airline.help.common.AbstractPrintedCommandGroupUsageGenerator;
+import com.github.rvesse.airline.io.printers.UsagePrinter;
+import com.github.rvesse.airline.model.CommandGroupMetadata;
+import com.github.rvesse.airline.model.CommandMetadata;
+import com.github.rvesse.airline.model.GlobalMetadata;
+import com.github.rvesse.airline.model.OptionMetadata;
+import com.github.rvesse.airline.utils.AirlineUtils;
+
+import static com.github.rvesse.airline.help.UsageHelper.DEFAULT_COMMAND_COMPARATOR;
+import static com.github.rvesse.airline.help.UsageHelper.DEFAULT_OPTION_COMPARATOR;
+
+public class MarkdownCommandGroupUsageGenerator<T> extends AbstractPrintedCommandGroupUsageGenerator<T> {
+    private final boolean hideGlobalOptions;
+
+    public MarkdownCommandGroupUsageGenerator() {
+        this(DEFAULT_COLUMNS, false, DEFAULT_OPTION_COMPARATOR, DEFAULT_COMMAND_COMPARATOR, false);
+    }
+
+    public MarkdownCommandGroupUsageGenerator(boolean includeHidden) {
+        this(DEFAULT_COLUMNS, false, DEFAULT_OPTION_COMPARATOR, DEFAULT_COMMAND_COMPARATOR, includeHidden);
+    }
+
+    public MarkdownCommandGroupUsageGenerator(int columns) {
+        this(columns, false, DEFAULT_OPTION_COMPARATOR, DEFAULT_COMMAND_COMPARATOR, false);
+    }
+
+    public MarkdownCommandGroupUsageGenerator(int columns, boolean includeHidden) {
+        this(columns, false, DEFAULT_OPTION_COMPARATOR, DEFAULT_COMMAND_COMPARATOR, includeHidden);
+    }
+
+    public MarkdownCommandGroupUsageGenerator(int columns, boolean hideGlobalOptions,
+            Comparator<? super OptionMetadata> optionComparator, Comparator<? super CommandMetadata> commandComparator,
+            boolean includeHidden) {
+        super(columns, optionComparator, commandComparator, includeHidden);
+        this.hideGlobalOptions = hideGlobalOptions;
+    }
+
+    @Override
+    protected void usage(GlobalMetadata<T> global, CommandGroupMetadata[] groups, UsagePrinter out) throws IOException {
+        // Description and Name
+        outputDescription(out, global, groups);
+
+        //
+        // SYNOPSIS
+        //
+        outputSynopsis(out, global, groups);
+
+        //
+        // OPTIONS
+        //
+        outputOptions(out, global, groups);
+    }
+
+    /**
+     * Outputs a documentation section detailing the available options and their
+     * usages
+     * 
+     * @param out
+     *            Usage printer
+     * @param global
+     *            Global meta-data
+     * @param groups
+     *            Group(s) meta-data
+     * 
+     * @throws IOException
+     */
+    protected void outputOptions(UsagePrinter out, GlobalMetadata<T> global, CommandGroupMetadata[] groups)
+            throws IOException {
+        List<OptionMetadata> options = new ArrayList<>();
+        options.addAll(groups[groups.length - 1].getOptions());
+        if (global != null && !hideGlobalOptions) {
+            options.addAll(global.getOptions());
+        }
+        if (options.size() > 0) {
+            options = sortOptions(options);
+
+            out.append("OPTIONS").newline();
+
+            for (OptionMetadata option : options) {
+
+                if (option.isHidden() && !this.includeHidden()) {
+                    continue;
+                }
+
+                // option names
+                UsagePrinter optionPrinter = out.newIndentedPrinter(8);
+                optionPrinter.append(toDescription(option)).newline();
+
+                // description
+                UsagePrinter descriptionPrinter = optionPrinter.newIndentedPrinter(4);
+                descriptionPrinter.append(option.getDescription()).newline();
+
+                descriptionPrinter.newline();
+            }
+        }
+    }
+
+    /**
+     * Outputs a documentation section detailing a usage synopsis
+     * 
+     * @param out
+     *            Usage printer
+     * @param global
+     *            Global meta-data
+     * @param group
+     *            Group meta-data
+     * @throws IOException
+     */
+    protected void outputSynopsis(UsagePrinter out, GlobalMetadata<T> global, CommandGroupMetadata[] groups)
+            throws IOException {
+        out.append("SYNOPSIS").newline();
+        UsagePrinter synopsis = out.newIndentedPrinter(8).newPrinterWithHangingIndent(8);
+
+        CommandGroupMetadata group = groups[groups.length - 1];
+        List<CommandMetadata> commands = sortCommands(group.getCommands());
+
+        // Populate group info via an extra for loop through commands
+        boolean hasDefaultCommand = group.getDefaultCommand() != null;
+
+        List<OptionMetadata> commonGroupOptions = null;
+        String commonGroupArgs = null;
+        List<String> allCommandNames = new ArrayList<>();
+        List<String> groupNames = new ArrayList<>();
+        boolean hasCommandSpecificOptions = false, hasCommandSpecificArgs = false;
+        for (CommandMetadata command : commands) {
+            if (group.getDefaultCommand() != null && group.getDefaultCommand().equals(command)) {
+                allCommandNames.add(command.getName() + "*");
+            } else {
+                allCommandNames.add(command.getName());
+            }
+            if (commonGroupOptions == null) {
+                commonGroupOptions = new ArrayList<>(command.getCommandOptions());
+            }
+            if (commonGroupArgs == null) {
+                commonGroupArgs = (command.getArguments() != null ? toUsage(command.getArguments()) : "");
+            }
+
+            commonGroupOptions.retainAll(command.getCommandOptions());
+            if (command.getCommandOptions().size() > commonGroupOptions.size()) {
+                hasCommandSpecificOptions = true;
+            }
+            if (commonGroupArgs != (command.getArguments() != null ? toUsage(command.getArguments()) : "")) {
+                hasCommandSpecificArgs = true;
+            }
+        }
+        for (CommandGroupMetadata subGroup : group.getSubGroups()) {
+            groupNames.add(subGroup.getName());
+            if (commonGroupOptions == null) {
+                commonGroupOptions = new ArrayList<>(subGroup.getOptions());
+            }
+            commonGroupOptions.retainAll(subGroup.getOptions());
+        }
+        // Print group summary line
+        if (global != null) {
+            synopsis.append(global.getName());
+            if (!hideGlobalOptions && commands.size() > 0) {
+                synopsis.appendWords(toSynopsisUsage(commands.get(0).getGlobalOptions()));
+            }
+        }
+        for (int i = 0; i < groups.length; i++) {
+            synopsis.append(groups[i].getName()).append(" ");
+        }
+        if (commands.size() > 0) {
+            synopsis.appendWords(toSynopsisUsage(commands.get(0).getGroupOptions()));
+        }
+        synopsis.append(" {");
+        if (allCommandNames.size() > 0) {
+            if (groupNames.size() > 0)
+                synopsis.append(" {");
+            for (int i = 0; i < allCommandNames.size(); i++) {
+                synopsis.append(allCommandNames.get(i));
+                if (i < allCommandNames.size() - 1)
+                    synopsis.append(" | ");
+            }
+        }
+        if (groupNames.size() > 0) {
+            if (allCommandNames.size() > 0)
+                synopsis.append("} | {");
+            for (int i = 0; i < groupNames.size(); i++) {
+                synopsis.append(groupNames.get(i)).append(" <sub-command>");
+                if (i < groupNames.size() - 1)
+                    synopsis.append(" | ");
+            }
+        }
+        synopsis.append("} [--]");
+        if (commonGroupOptions.size() > 0) {
+            synopsis.appendWords(toSynopsisUsage(commonGroupOptions));
+        }
+        if (hasCommandSpecificOptions) {
+            synopsis.append(" [cmd-options]");
+        }
+        if (hasCommandSpecificArgs) {
+            synopsis.append(" <cmd-args>");
+        }
+        synopsis.newline();
+        Map<String, String> cmdOptions = new TreeMap<>();
+        Map<String, String> cmdArguments = new TreeMap<>();
+        Map<String, String> subGroups = new TreeMap<>();
+
+        for (CommandGroupMetadata subGroup : group.getSubGroups()) {
+            if (!subGroup.isHidden() || this.includeHidden()) {
+                StringBuilder groupSB = new StringBuilder();
+                for (CommandGroupMetadata subSubGroup : subGroup.getSubGroups()) {
+                    if (groupSB.length() > 0)
+                        groupSB.append(", ");
+                    groupSB.append(subSubGroup.getName()).append(' ').append("<sub-command>");
+                }
+                for (CommandMetadata subCommand : subGroup.getCommands()) {
+                    if (groupSB.length() > 0)
+                        groupSB.append(", ");
+                    groupSB.append(subCommand.getName());
+                    if (subGroup.getDefaultCommand() != null && subGroup.getDefaultCommand().equals(subCommand)) {
+                        groupSB.append("*");
+                        hasDefaultCommand = true;
+                    }
+                }
+                subGroups.put(subGroup.getName(), groupSB.toString());
+            }
+        }
+
+        if (subGroups.size() > 0) {
+            synopsis.newline().append("Where command groups contain the following sub-groups and commands:").newline();
+            UsagePrinter grps = synopsis.newIndentedPrinter(4);
+            for (String groupName : subGroups.keySet()) {
+                grps.append(groupName + ": " + subGroups.get(groupName)).newline();
+            }
+        }
+
+        for (CommandMetadata command : commands) {
+
+            if (!command.isHidden() || this.includeHidden()) {
+                if (hasCommandSpecificOptions) {
+                    List<OptionMetadata> thisCmdOptions = new ArrayList<>(command.getCommandOptions());
+                    thisCmdOptions.removeAll(commonGroupOptions);
+                    StringBuilder optSB = new StringBuilder();
+                    for (String s : toSynopsisUsage(thisCmdOptions)) {
+                        optSB.append(s + " ");
+                    }
+                    cmdOptions.put(command.getName(), optSB.toString());
+                }
+                if (hasCommandSpecificArgs) {
+                    cmdArguments.put(command.getName(),
+                            (command.getArguments() != null ? toUsage(command.getArguments()) : ""));
+                }
+            }
+        }
+        if (hasCommandSpecificOptions) {
+            synopsis.newline().append("Where command-specific options [cmd-options] are:").newline();
+            UsagePrinter opts = synopsis.newIndentedPrinter(4);
+            for (String cmd : cmdOptions.keySet()) {
+                opts.append(cmd + ": " + cmdOptions.get(cmd)).newline();
+            }
+        }
+        if (hasCommandSpecificArgs) {
+            synopsis.newline().append("Where command-specific arguments <cmd-args> are:").newline();
+            UsagePrinter args = synopsis.newIndentedPrinter(4);
+            for (String arg : cmdArguments.keySet()) {
+                args.append(arg + ": " + cmdArguments.get(arg)).newline();
+            }
+        }
+        if (hasDefaultCommand) {
+            synopsis.newline().append("Where * indicates the default command(s)");
+        }
+        synopsis.newline().append("See").append("'" + global.getName()).append("help ")
+                .appendWords(UsageHelper.toGroupNames(AirlineUtils.arrayToList(groups)))
+                .appendOnOneLine(" <command>' for more information on a specific command.").newline();
+    }
+
+    /**
+     * Outputs a description of the group
+     * 
+     * @param out
+     *            Usage printer
+     * @param global
+     *            Global meta-data
+     * @param group
+     *            Group meta-data
+     * @throws IOException
+     */
+    protected void outputDescription(UsagePrinter out, GlobalMetadata<T> global, CommandGroupMetadata[] groups)
+            throws IOException {
+        out.append("NAME").newline();
+
+        out.newIndentedPrinter(8).append(global.getName()).append(" ");
+        CommandGroupMetadata group = null;
+        for (int i = 0; i < groups.length; i++) {
+            group = groups[i];
+            out.append(group.getName());
+            if (i < groups.length - 1)
+                out.append(" ");
+        }
+        out.append(" - ").append(group.getDescription()).newline().newline();
+    }
+}
---
+++ airline-help/airline-help-markdown/src/main/java/com/github/rvesse/airline/help/markdown/MarkdownUsageHelper.java
@@ -0,0 +1,267 @@
+/**
+ * Copyright (C) 2010-15 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.rvesse.airline.help.markdown;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.github.rvesse.airline.help.UsageHelper;
+import com.github.rvesse.airline.help.common.AbstractUsageGenerator;
+import com.github.rvesse.airline.help.sections.HelpFormat;
+import com.github.rvesse.airline.help.sections.HelpHint;
+import com.github.rvesse.airline.help.sections.HelpSection;
+import com.github.rvesse.airline.io.printers.UsagePrinter;
+import com.github.rvesse.airline.model.ArgumentsMetadata;
+import com.github.rvesse.airline.model.OptionMetadata;
+import com.github.rvesse.airline.model.ParserMetadata;
+import com.github.rvesse.airline.restrictions.ArgumentsRestriction;
+import com.github.rvesse.airline.restrictions.OptionRestriction;
+
+public class MarkdownUsageHelper extends AbstractUsageGenerator {
+
+    public MarkdownUsageHelper(Comparator<? super OptionMetadata> optionComparator, boolean includeHidden) {
+        super(optionComparator, UsageHelper.DEFAULT_COMMAND_COMPARATOR, includeHidden);
+    }
+
+    public void outputOptions(UsagePrinter out, List<OptionMetadata> options) throws IOException {
+        out.append("# OPTIONS").newline();
+
+        options = sortOptions(options);
+        for (OptionMetadata option : options) {
+            // Skip hidden options
+            if (option.isHidden() && !this.includeHidden()) {
+                continue;
+            }
+
+            // Option names
+            UsagePrinter optionPrinter = out.newIndentedPrinter(8);
+            optionPrinter.append(toDescription(option)).newline();
+            optionPrinter.flush();
+
+            // Description
+            UsagePrinter descriptionPrinter = optionPrinter.newIndentedPrinter(4);
+            descriptionPrinter.append(option.getDescription()).newline();
+
+            // Restrictions
+            for (OptionRestriction restriction : option.getRestrictions()) {
+                if (restriction instanceof HelpHint) {
+                    outputOptionRestriction(descriptionPrinter, option, restriction, (HelpHint) restriction);
+                }
+            }
+
+            descriptionPrinter.newline();
+            descriptionPrinter.flush();
+        }
+    }
+
+    /**
+     * Outputs documentation about a restriction on an option
+     * 
+     * @param out
+     *            Usage printer
+     * @param option
+     *            Option meta-data
+     * @param restriction
+     *            Restriction
+     * @param hint
+     *            Help hint
+     * @throws IOException
+     */
+    protected void outputOptionRestriction(UsagePrinter out, OptionMetadata option, OptionRestriction restriction,
+            HelpHint hint) throws IOException {
+        out.newline();
+        outputHint(out, hint, false);
+    }
+
+    protected void outputHint(UsagePrinter out, HelpHint hint, boolean newPara) throws IOException {
+        // Ignore non-printable help
+        if (hint.getFormat() == HelpFormat.NONE_PRINTABLE)
+            return;
+        
+        // Print preamble if present
+        if (!StringUtils.isBlank(hint.getPreamble())) {
+            out.append(hint.getPreamble());
+            out.newline();
+            if (newPara)
+                out.newline();
+        }
+        out.flush();
+
+        // Print more details if there are some content blocks present
+        if (hint.numContentBlocks() > 0) {
+            // Decide how to print based on the format
+            int maxRows;
+            switch (hint.getFormat()) {
+            case EXAMPLES:
+                // Print as text with indents
+                for (int e = 0; e < hint.getContentBlock(0).length; e++) {
+                    // Example will be in first content block
+                    out.appendOnOneLine(hint.getContentBlock(0)[e]);
+                    out.newline().newline();
+                    out.flush();
+
+                    // Print example description with additional indent
+                    UsagePrinter examplePrinter = out.newIndentedPrinter(4);
+                    for (int d = 1; d < hint.numContentBlocks(); d++) {
+                        String[] descriptions = hint.getContentBlock(d);
+                        if (e >= descriptions.length)
+                            continue;
+                        examplePrinter.append(descriptions[e]);
+                        examplePrinter.newline().newline();
+                    }
+                    examplePrinter.flush();
+                }
+                break;
+            case TABLE:
+            case TABLE_WITH_HEADERS:
+                // Print as table
+                // Convert to form that appendTable() understands
+                // i.e. columns -> rows
+                maxRows = calculateMaxRows(hint);
+                List<List<String>> rows = new ArrayList<List<String>>();
+                for (int row = 0; row < maxRows; row++) {
+                    List<String> rowData = new ArrayList<String>();
+                    for (int col = 0; col < hint.numContentBlocks(); col++) {
+                        String[] colData = hint.getContentBlock(col);
+                        rowData.add(row < colData.length ? colData[row] : null);
+                    }
+                    rows.add(rowData);
+                }
+
+                // Print out table
+                UsagePrinter tablePrinter = out.newIndentedPrinter(4);
+                tablePrinter.appendTable(rows, 0);
+                tablePrinter.newline();
+                tablePrinter.flush();
+                break;
+            case LIST:
+                // Print first content block as an indented list list
+                UsagePrinter listPrinter = out.newIndentedPrinter(4);
+                for (String item : hint.getContentBlock(0)) {
+                    listPrinter.append(item).newline();
+                }
+                listPrinter.flush();
+                break;
+            default:
+                // Print content blocks as text
+                for (int i = 0; i < hint.numContentBlocks(); i++) {
+                    for (String line : hint.getContentBlock(i)) {
+                        out.append(line);
+                        out.newline().newline();
+                    }
+                }
+                break;
+            }
+        }
+    }
+
+    public static int calculateMaxRows(HelpHint hint) {
+        int maxRows = 0;
+        for (int col = 0; col < hint.numContentBlocks(); col++) {
+            maxRows = Math.max(maxRows, hint.getContentBlock(col).length);
+        }
+        return maxRows;
+    }
+
+    public <T> void outputArguments(UsagePrinter out, ArgumentsMetadata arguments, ParserMetadata<T> parserConfig) throws IOException {
+        if (arguments != null) {
+            // Arguments separator option
+            UsagePrinter optionPrinter = out.newIndentedPrinter(8);
+            optionPrinter.append(parserConfig.getArgumentsSeparator()).newline();
+            optionPrinter.flush();
+
+            // Description
+            UsagePrinter descriptionPrinter = optionPrinter.newIndentedPrinter(4);
+            descriptionPrinter
+                    .append("This option can be used to separate command-line options from the list of argument (useful when arguments might be mistaken for command-line options)")
+                    .newline();
+            descriptionPrinter.newline();
+
+            // Arguments name(s)
+            optionPrinter.append(toDescription(arguments)).newline();
+
+            // Description
+            descriptionPrinter.append(arguments.getDescription()).newline();
+
+            // Restrictions
+            for (ArgumentsRestriction restriction : arguments.getRestrictions()) {
+                if (restriction instanceof HelpHint) {
+                    outputArgumentsRestriction(descriptionPrinter, arguments, restriction, (HelpHint) restriction);
+                }
+            }
+
+            descriptionPrinter.newline();
+            descriptionPrinter.flush();
+        }
+    }
+
+    /**
+     * Outputs documentation about a restriction on an option
+     * 
+     * @param descriptionPrinter
+     *            Description printer
+     * @param arguments
+     *            Arguments meta-data
+     * @param restriction
+     *            Restriction
+     * @param hint
+     *            Help hint
+     * @throws IOException
+     */
+    protected void outputArgumentsRestriction(UsagePrinter descriptionPrinter, ArgumentsMetadata arguments,
+            ArgumentsRestriction restriction, HelpHint hint) throws IOException {
+        descriptionPrinter.newline();
+        outputHint(descriptionPrinter, hint, false);
+    }
+
+    /**
+     * Outputs a help section
+     * 
+     * @param out
+     *            Usage printer
+     * @param section
+     *            Help section
+     * @throws IOException
+     */
+    public void outputHelpSection(UsagePrinter out, HelpSection section) throws IOException {
+        if (section.getFormat() == HelpFormat.NONE_PRINTABLE)
+            return;
+
+        // Section title
+        if (!StringUtils.isBlank(section.getTitle())) {
+            out.append(section.getTitle().toUpperCase());
+            out.newline();
+        }
+
+        UsagePrinter sectionPrinter = out.newIndentedPrinter(8);
+
+        // Content
+        outputHint(sectionPrinter, section, true);
+
+        // Post-amble
+        if (!StringUtils.isBlank(section.getPostamble())) {
+            sectionPrinter.append(section.getPostamble());
+            sectionPrinter.newline();
+        }
+
+        sectionPrinter.flush();
+        out.flush();
+    }
+}
