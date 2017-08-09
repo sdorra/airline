42de4139e1eabb0aead0492fc2f9db15f9fba0bc
Rob Vesse <rvesse@dotnetrdf.org>
2017-08-09 13:52:04 +0100

    Get build working again (#45)

--- airline-core/src/main/java/com/github/rvesse/airline/help/common/AbstractUsageGenerator.java
+++ airline-core/src/main/java/com/github/rvesse/airline/help/common/AbstractUsageGenerator.java
@@ -34,7 +34,7 @@ import com.github.rvesse.airline.restrictions.OptionRestriction;
 
 public class AbstractUsageGenerator {
 
-    protected static final int DEFAULT_COLUMNS = 79;
+    public static final int DEFAULT_COLUMNS = 79;
     private final Comparator<? super HelpHint> hintComparator;
     private final Comparator<? super OptionMetadata> optionComparator;
     private final Comparator<? super CommandMetadata> commandComparator;
